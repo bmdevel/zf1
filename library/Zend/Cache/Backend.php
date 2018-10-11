5f8818ee8e0941ea5fa329e85b6c0b7a47e56ac8
Johannes Jambor <jambor@billiger-mietwagen.de>
2018-10-11 14:07:57 +0200

    fix: use foreach() instead of each()

--- library/Zend/Cache/Backend.php
+++ library/Zend/Cache/Backend.php
@@ -59,11 +59,10 @@ class Zend_Cache_Backend
      *
      * @param  array $options Associative array of options
      * @throws Zend_Cache_Exception
-     * @return void
      */
     public function __construct(array $options = array())
     {
-        while (list($name, $value) = each($options)) {
+        foreach ($options as $name => $value) {
             $this->setOption($name, $value);
         }
     }
@@ -78,7 +77,7 @@ class Zend_Cache_Backend
     public function setDirectives($directives)
     {
         if (!is_array($directives)) Zend_Cache::throwException('Directives parameter must be an array');
-        while (list($name, $value) = each($directives)) {
+        foreach ($directives as $name => $value) {
             if (!is_string($name)) {
                 Zend_Cache::throwException("Incorrect option name : $name");
             }
@@ -86,7 +85,6 @@ class Zend_Cache_Backend
             if (array_key_exists($name, $this->_directives)) {
                 $this->_directives[$name] = $value;
             }
-
         }
 
         $this->_loggerSanity();
