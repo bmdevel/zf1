4ecb1df2dc13f554b4e462e3c2e5f0484d200796
Dominik Peters <d.peters@billiger-mietwagen.de>
2020-10-30 17:05:06 +0100

    remove 'track_errors' ini directive usage

--- library/Zend/Feed.php
+++ library/Zend/Feed.php
@@ -257,15 +257,13 @@ class Zend_Feed
      */
     public static function importFile($filename)
     {
-        @ini_set('track_errors', 1);
         $feed = @file_get_contents($filename);
-        @ini_restore('track_errors');
         if ($feed === false) {
             /**
              * @see Zend_Feed_Exception
              */
             require_once 'Zend/Feed/Exception.php';
-            throw new Zend_Feed_Exception("File could not be loaded: $php_errormsg");
+            throw new Zend_Feed_Exception("File could not be loaded");
         }
         return self::importString($feed);
     }
@@ -297,16 +295,14 @@ class Zend_Feed
         $contents = $response->getBody();
 
         // Parse the contents for appropriate <link ... /> tags
-        @ini_set('track_errors', 1);
         $pattern = '~(<link[^>]+)/?>~i';
         $result = @preg_match_all($pattern, $contents, $matches);
-        @ini_restore('track_errors');
         if ($result === false) {
             /**
              * @see Zend_Feed_Exception
              */
             require_once 'Zend/Feed/Exception.php';
-            throw new Zend_Feed_Exception("Internal error: $php_errormsg");
+            throw new Zend_Feed_Exception("Internal error");
         }
 
         // Try to fetch a feed for each link tag that appears to refer to a feed
--- library/Zend/Gdata/App/Base.php
+++ library/Zend/Gdata/App/Base.php
@@ -302,13 +302,11 @@ abstract class Zend_Gdata_App_Base
     {
         if ($xml) {
             // Load the feed as an XML DOMDocument object
-            @ini_set('track_errors', 1);
             $doc = new DOMDocument();
             $doc = @Zend_Xml_Security::scan($xml, $doc);
-            @ini_restore('track_errors');
             if (!$doc) {
                 require_once 'Zend/Gdata/App/Exception.php';
-                throw new Zend_Gdata_App_Exception("DOMDocument cannot parse XML: $php_errormsg");
+                throw new Zend_Gdata_App_Exception("DOMDocument cannot parse XML");
             }
             $element = $doc->getElementsByTagName($this->_rootElement)->item(0);
             if (!$element) {
